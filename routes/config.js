ca22ee2f5ffd013ce567490ea1c046928e9451ff
Martin Conti <matin.a.conti2014@gmail.com>
2024-06-22 00:03:03 -0300

    vercel.json

--- routes/config.js
+++ routes/config.js
@@ -2,7 +2,7 @@ const { config } = require('dotenv');
 
 config()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 const DB_HOST = process.env.DB_HOST 
 const DB_PORT = process.env.DB_PORT
 const DB_USER = process.env.DB_USER
